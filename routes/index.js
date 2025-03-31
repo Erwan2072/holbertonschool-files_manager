e115ea2f62a3962bde5aaaedb6f441f93e069fc2
Erwan2072 <erwan.lebreton@hotmail.fr>
2025-03-31 18:35:47 +0200

    merge index.js and FilesController from Erwan branch

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,18 @@
 import express from 'express';
 import UsersController from '../controllers/UsersController';
 import AppController from '../controllers/AppController';
+import FilesController from '../controllers/FilesController';
+import AuthController from '../controllers/AuthController';
+
 
 const router = express.Router();
 
 router.get('/status', AppController.getStatus);
 router.get('/stats', AppController.getStats);
+router.get('/connect', AuthController.getConnect);
+router.get('/disconnect', AuthController.getDisconnect);
+router.get('/users/me', UsersController.getMe);
 router.post('/users', UsersController.postNew);
+router.post('/files', FilesController.postUpload);
 
 export default router;
