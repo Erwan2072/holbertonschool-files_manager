1b9e31178d5ecc3da437299903513582c2649a78
Erwan2072 <erwan.lebreton@hotmail.fr>
2025-03-31 17:16:21 +0200

    correction file index and server

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,12 @@
+// routes/index.js
 import express from 'express';
 import UsersController from '../controllers/UsersController';
+import AppController from '../controllers/AppController';
 
 const router = express.Router();
+
+router.get('/status', AppController.getStatus);
+router.get('/stats', AppController.getStats);
 router.post('/users', UsersController.postNew);
+
 export default router;
