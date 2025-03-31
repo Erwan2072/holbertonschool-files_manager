5ff683ae22bca68ba647e4bab05ea3c8a7493154
Erwan2072 <erwan.lebreton@hotmail.fr>
2025-03-31 16:56:44 +0200

    Add POST /users to create new users with email and hashed password

---
+++ routes/index.js
@@ -0,0 +1,6 @@
+import express from 'express';
+import UsersController from '../controllers/UsersController';
+
+const router = express.Router();
+router.post('/users', UsersController.postNew);
+export default router;
--- utils/db.mjs
+++ utils/db.mjs
@@ -1,18 +1,18 @@
 // utils/db.mjs
 
-import { MongoClient } from 'mongodb';
+import mongodb from 'mongodb';
 
-// Paramètres de connexion
+const { MongoClient } = mongodb;
+
 const host = process.env.DB_HOST || 'localhost';
 const port = process.env.DB_PORT || 27017;
 const database = process.env.DB_DATABASE || 'files_manager';
-
 const url = `mongodb://${host}:${port}`;
 
 class DBClient {
   constructor() {
     this.client = new MongoClient(url, { useUnifiedTopology: true });
-
+    this.db = null;
     this.client.connect()
       .then(() => {
         this.db = this.client.db(database);
@@ -28,14 +28,15 @@ class DBClient {
   }
 
   async nbUsers() {
-    return this.db?.collection('users')?.countDocuments() || 0;
+    if (!this.db) return 0;
+    return this.db.collection('users').countDocuments();
   }
 
   async nbFiles() {
-    return this.db?.collection('files')?.countDocuments() || 0;
+    if (!this.db) return 0;
+    return this.db.collection('files').countDocuments();
   }
 }
 
-// Export de l’instance
 const dbClient = new DBClient();
 export default dbClient;
