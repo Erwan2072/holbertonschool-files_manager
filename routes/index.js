2c8ab285198e7c340d26156d97ce213443b4fe35
Erwan2072 <erwan.lebreton@hotmail.fr>
2025-03-31 17:02:20 +0200

    Bring back 2 files from Erwan branch

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,6 @@
 import express from 'express';
-import AppController from '../controllers/AppController';
+import UsersController from '../controllers/UsersController';
 
 const router = express.Router();
-
-router.get('/status', (req, res) => AppController.getStatus(req, res));
-
-router.get('/stats', (req, res) => AppController.getStats(req, res));
-
+router.post('/users', UsersController.postNew);
 export default router;
