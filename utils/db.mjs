ecc91dd1c5312f44cc283714c52a30fa92ef2091
Erwan2072 <erwan.lebreton@hotmail.fr>
2025-03-31 14:05:48 +0200

    correction file db

--- utils/db.mjs
+++ utils/db.mjs
@@ -1,25 +1,21 @@
 // utils/db.mjs
-
 import { MongoClient } from 'mongodb';
 
-// Paramètres de connexion
 const host = process.env.DB_HOST || 'localhost';
 const port = process.env.DB_PORT || 27017;
 const database = process.env.DB_DATABASE || 'files_manager';
-
 const url = `mongodb://${host}:${port}`;
 
 class DBClient {
   constructor() {
     this.client = new MongoClient(url, { useUnifiedTopology: true });
-
+    this.db = null;
     this.client.connect()
       .then(() => {
         this.db = this.client.db(database);
       })
       .catch((err) => {
         console.error('MongoDB connection error:', err);
-        this.db = null;
       });
   }
 
@@ -28,14 +24,17 @@ class DBClient {
   }
 
   async nbUsers() {
-    return this.db?.collection('users')?.countDocuments() || 0;
+    if (!this.db) return 0;
+    const users = await this.db.collection('users').countDocuments();
+    return users;
   }
 
   async nbFiles() {
-    return this.db?.collection('files')?.countDocuments() || 0;
+    if (!this.db) return 0;
+    const files = await this.db.collection('files').countDocuments();
+    return files;
   }
 }
 
-// Export de l’instance
 const dbClient = new DBClient();
 export default dbClient;
