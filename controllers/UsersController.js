e86786f3d3152c4d2a29eac4cc4b5ef9c2396a0f
VandevilleF <flo.vandeville@gmail.com>
2025-04-01 20:28:08 +0200

    Fix ESlint........

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -31,6 +31,7 @@ class UsersController {
       return res.status(500).json({ error: 'Server error' });
     }
   }
+
   static async getMe(req, res) {
     const token = req.headers['x-token'] || req.headers['X-Token'];
 
