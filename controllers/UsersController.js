81a0231dbfab1d8dfb10f27bddf3b6eec891576c
Erwan2072 <erwan.lebreton@hotmail.fr>
2025-04-03 10:36:01 +0200

    modification Files controllers

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,6 +1,8 @@
 // controllers/UsersController.js
 import sha1 from 'sha1';
+import { ObjectId } from 'mongodb';
 import dbClient from '../utils/db';
+import redisClient from '../utils/redis';
 
 class UsersController {
   static async postNew(req, res) {
@@ -28,6 +30,30 @@ class UsersController {
     } catch (err) {
       return res.status(500).json({ error: 'Server error' });
     }
+  }
+
+  static async getMe(req, res) {
+    const token = req.headers['x-token'] || req.headers['X-Token'];
+
+    if (!token) {
+      res.status(401).json({ error: 'Unauthorized' });
+    }
+
+    const key = `auth_${token}`;
+    const userId = await redisClient.get(key);
+
+    if (!userId) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+
+    const usersCollection = dbClient.db.collection('users');
+    const user = await usersCollection.findOne({ _id: ObjectId(userId) });
+
+    if (!user) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+
+    return res.status(200).json({ id: user._id, email: user.email });
   }
 }
 
