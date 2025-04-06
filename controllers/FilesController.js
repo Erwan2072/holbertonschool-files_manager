5dc0445674cdd588c277a3b55f58ceeb50a6b839
Erwan2072 <erwan.lebreton@hotmail.fr>
2025-04-06 21:56:03 +0200

    fix ESlint part 2

--- controllers/FilesController.js
+++ controllers/FilesController.js
@@ -6,7 +6,6 @@ import redisClient from '../utils/redis';
 import dbClient from '../utils/db';
 import mime from 'mime-types';
 
-
 const folderPath = process.env.FOLDER_PATH || '/tmp/files_manager';
 const validFileTypes = ['folder', 'file', 'image'];
 
@@ -214,13 +213,13 @@ class FilesController {
   static async getFile(req, res) {
     const token = req.headers['x-token'] || req.headers['X-Token'];
     const fileId = req.params.id;
-  
+
     try {
       const file = await dbClient.db.collection('files').findOne({ _id: new ObjectId(fileId) });
       if (!file) {
         return res.status(404).json({ error: 'Not found' });
       }
-  
+
       if (!file.isPublic) {
         if (!token) return res.status(404).json({ error: 'Not found' });
         const userId = await redisClient.get(`auth_${token}`);
@@ -228,15 +227,15 @@ class FilesController {
           return res.status(404).json({ error: 'Not found' });
         }
       }
-  
+
       if (file.type === 'folder') {
         return res.status(400).json({ error: "A folder doesn't have content" });
       }
-  
+
       if (!file.localPath || !fs.existsSync(file.localPath)) {
         return res.status(404).json({ error: 'Not found' });
       }
-  
+
       const mimeType = mime.lookup(file.name) || 'application/octet-stream';
       res.setHeader('Content-Type', mimeType);
       const content = fs.readFileSync(file.localPath);
@@ -244,7 +243,7 @@ class FilesController {
     } catch (error) {
       return res.status(404).json({ error: 'Not found' });
     }
-  }  
+  }
 }
 
 export default FilesController;
